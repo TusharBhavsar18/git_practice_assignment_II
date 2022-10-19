2e790d0106541c112b9fc64f74851d3d275e44a0
TusharBhavsar18 <tusharbhavsar2011@gmail.com>
2022-10-19 15:00:16 +0530

    prime & palindrome

---
+++ Palindrome_Problem.js
@@ -0,0 +1,19 @@
+Check_Palindrome...
+
+	let str = "naman";
+    
+    let bag += "";
+    
+    for(let p=str.lenght-1 ; p>=0 ; p--){
+        
+        bag = bog-str[p];
+        
+    }if(bag=str){
+        
+        console.log("Yes");
+        
+    }else{
+    
+    consolelog("No");
+
+}
---
+++ Prime_Problem.js
@@ -0,0 +1,18 @@
+Identify_Prime...
+
+let num = 13;
+
+let count = 0;
+    
+    for(let l=0 ; i<=num ; i++){
+        
+        if(num%i==0){
+            
+            count++;
+        }
+    }if(count==1){
+        
+        cosole.log("Yes");
+    }else{
+        console.log("No");
+    }
